f620ec7dd02198d9d5d3a810cf4e41a85b09addb
TomiB98 <tomasbaldi@gmail.com>
2023-08-01 20:16:00 -0300

    Ruta detail

--- server/src/controllers/getProductDetail.js
+++ server/src/controllers/getProductDetail.js
@@ -2,21 +2,41 @@ const productsData = require('../data')
 
 const getProductDetail = async (id) => {
 
-    productsData.find((product) => {
-        if (product.id === id) {
-            return {
-                id: product.id,
-                href: product.href,
-                imageSrc: product.imageSrc,
-                imageAlt: product.imageAlt,
-                price: product.price,
-                stock: product.stock,
-                brand: product.brand,
-                category: product.category,
-                description: product.description
-            };
-        }
-    })
+    let findProduct = productsData.find(product => product.id === id);
+
+    if (findProduct) {
+        return {
+            id: findProduct.id,
+            href: findProduct.href,
+            imageSrc: findProduct.imageSrc,
+            imageAlt: findProduct.imageAlt,
+            price: findProduct.price,
+            stock: findProduct.stock,
+            brand: findProduct.brand,
+            category: findProduct.category,
+            description: findProduct.description
+        };
+    }
+    return null;
+
+
+    // let productId = productsData.find((product) => {
+    //     if (product.id === id) {
+    //         return {
+    //             id: product.id,
+    //             href: product.href,
+    //             imageSrc: product.imageSrc,
+    //             imageAlt: product.imageAlt,
+    //             price: product.price,
+    //             stock: product.stock,
+    //             brand: product.brand,
+    //             category: product.category,
+    //             description: product.description
+    //         };
+    //     }
+
+    // })
+    // return productId
 }
 // {
 //     return {
