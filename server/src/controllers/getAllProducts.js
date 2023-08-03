c391c2cde2f8870a9053a9345d09a1b4417940bd
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-03 10:38:25 -0300

    serachbar(fronet/back)

--- server/src/controllers/getAllProducts.js
+++ server/src/controllers/getAllProducts.js
@@ -1,41 +1,42 @@
-const productsData = require('../data')
+const productsData = require("../data");
 
 const getAllProducts = async () =>
-
-    productsData.map((product) => {
-        return {
-            id: product.id,
-            name: product.name,
-            href: product.href,
-            imageSrc: product.imageSrc,
-            imageAlt: product.imageAlt,
-            price: product.price,
-            stock: product.stock,
-            brand: product.brand,
-            category: product.category,
-            description: product.description
-        };
-    })
+  productsData.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      href: product.href,
+      imageSrc: product.imageSrc,
+      imageAlt: product.imageAlt,
+      price: product.price,
+      stock: product.stock,
+      brand: product.brand,
+      category: product.category,
+      description: product.description,
+    };
+  });
 
 const getProductByName = async (name) => {
+  const filteredProducts = productsData.filter((product) =>
+    product.name.toLowerCase().includes(name.toLowerCase())
+  );
 
-    let findProduct = productsData.find(product => product.name === name);
+  if (filteredProducts.length > 0) {
+    return filteredProducts.map((product) => ({
+      id: product.id,
+      name: product.name,
+      href: product.href,
+      imageSrc: product.imageSrc,
+      imageAlt: product.imageAlt,
+      price: product.price,
+      stock: product.stock,
+      brand: product.brand,
+      category: product.category,
+      description: product.description,
+    }));
+  }
 
-    if (findProduct) {
-        return {
-            id: findProduct.id,
-            name: findProduct.name,
-            href: findProduct.href,
-            imageSrc: findProduct.imageSrc,
-            imageAlt: findProduct.imageAlt,
-            price: findProduct.price,
-            stock: findProduct.stock,
-            brand: findProduct.brand,
-            category: findProduct.category,
-            description: findProduct.description
-        };
-    }
-    return null;
-}
+  return null;
+};
 
 module.exports = { getAllProducts, getProductByName };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,21 +1,21 @@
-const { Router } = require('express');
-const { getAllProductsHandler } = require('../handlers/getAllProductsHandler')
-const { getDetailHandler } = require('../handlers/getProductDetailHandler')
-const getDbProducts = require('../controllers/getDbProducts.js');
-const postCreateProduct = require('../controllers/postCreateProduct');
+const { Router } = require("express");
+const { getAllProductsHandler } = require("../handlers/getAllProductsHandler");
+const { getDetailHandler } = require("../handlers/getProductDetailHandler");
+const getDbProducts = require("../controllers/getDbProducts.js");
+const postCreateProduct = require("../controllers/postCreateProduct");
 
 const router = Router();
 
 //Ruta para mostrar los productos
-router.get('/productos', getAllProductsHandler)
+router.get("/productos", getAllProductsHandler);
 
 //Ruta para mostrar el detalle de los productos
-router.get('/productos/:id', getDetailHandler)
+router.get("/productos/:id", getDetailHandler);
 
 //Ruta para llenar la base de datos con los productos
-router.get('/dbproducts', getDbProducts);
+router.get("/dbproducts", getDbProducts);
 
 //Ruta para almacenar un nuevo producto al db
-router.post('/productos', postCreateProduct);
+router.post("/productos", postCreateProduct);
 
-module.exports = router
+module.exports = router;
