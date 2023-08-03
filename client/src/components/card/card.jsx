89b0e2960b8f6979e08a9854883ce3f07e52327f
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-03 17:02:11 -0300

    CSS(Body/Detail) Link(Detail)

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import style from "./card.module.css";
 // import { useDispatch } from "react-redux";
 // import { deleteProductsById } from "../../redux/actions";
@@ -21,18 +21,23 @@ export default function Card({
   // aca va const handleClose = () => { dispatch(deleteProductsById(id))}
   return (
     <div className={style.card}>
-      <div className={style.img_container}>
-        <img className={style.card__img} src={imageSrc} alt={imageAlt} />
-      </div>
-      <div className={style.container_name}>
-        <p className={style.ht}>{name}</p>
-      </div>
-      <div className={style.container_data}>
-        <span className={style.price}>${price}</span>
-        <p className={style.texts}>{brand}</p>
-        <p className={style.texts}>{category}</p>
-      </div>
-      <p className={style.stock}>Stock disponible {stock}</p>
+      <NavLink
+        to={`/product/${id}`}
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className={style.img_container}>
+          <img className={style.card__img} src={imageSrc} alt={imageAlt} />
+        </div>
+        <div className={style.container_name}>
+          <p className={style.ht}>{name}</p>
+        </div>
+        <div className={style.container_data}>
+          <span className={style.price}>${price}</span>
+          <p className={style.texts}>{brand}</p>
+          <p className={style.texts}>{category}</p>
+        </div>
+        <p className={style.stock}>Stock disponible {stock}</p>
+      </NavLink>
     </div>
   );
 }
--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -6,69 +6,71 @@ import { getProducts, orderByPrice, filterByBrand } from "../../redux/actions";
 
 const Home = () => {
   const products = useSelector((state) => state.allProducts);
-    
-  const dispatch = useDispatch()
+
+  const dispatch = useDispatch();
 
   //paginado
-  const productsPerPage = 6;
+  const productsPerPage = 8;
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-      dispatch(getProducts())
-  }, [dispatch])
+    dispatch(getProducts());
+  }, [dispatch]);
 
   const handleNextPage = () => {
-      setCurrentPage((nextPage) => nextPage + 1);
+    setCurrentPage((nextPage) => nextPage + 1);
   };
 
-    const handlePreviousPage = () => {
-        setCurrentPage((previousPage) => previousPage - 1);
-    };
+  const handlePreviousPage = () => {
+    setCurrentPage((previousPage) => previousPage - 1);
+  };
 
-    const startIndex = (currentPage - 1) * productsPerPage;
-    const endIndex = startIndex + productsPerPage;
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
 
-    const recipesToDisplay = products.slice(startIndex, endIndex);
+  const recipesToDisplay = products.slice(startIndex, endIndex);
 
-    //filtros
-    const HandlerOrderByPrice = (event) => {
-        dispatch(orderByPrice(event.target.value))
-    }
+  //filtros
+  const HandlerOrderByPrice = (event) => {
+    dispatch(orderByPrice(event.target.value));
+  };
 
-    const handleBrandFilter = (event) => {
-        const selectedBrand = event.target.value;
-        dispatch(filterByBrand(selectedBrand));
-      };
+  const handleBrandFilter = (event) => {
+    const selectedBrand = event.target.value;
+    dispatch(filterByBrand(selectedBrand));
+  };
 
-    return (
-        <div>
-            
-            <div>
-                <select onChange={HandlerOrderByPrice}>
-                    <option value="">Price</option>
-                    <option value="-+">Menor a Mayor</option>
-                    <option value="+-">Mayor a Menor</option>
-                </select>
-                <select onChange={handleBrandFilter}>
+  return (
+    <div>
+      <div>
+        <select onChange={HandlerOrderByPrice}>
+          <option value="">Price</option>
+          <option value="-+">Menor a Mayor</option>
+          <option value="+-">Mayor a Menor</option>
+        </select>
+        <select onChange={handleBrandFilter}>
           <option value="">Brand</option>
           <option value="Apple">Apple</option>
           <option value="Motorola">Motorola</option>
           <option value="Samsung">Samsung</option>
           <option value="xiaomi">xiaomi</option>
         </select>
-            </div>
+      </div>
 
-            <div>
-                <Cards products={recipesToDisplay}/>
-            </div>
+      <div>
+        <Cards products={recipesToDisplay} />
+      </div>
 
-            <div>
-                {currentPage > 1 && <button onClick={handlePreviousPage}>Previous</button>}
-                {endIndex < products.length && <button onClick={handleNextPage}>Next</button>}
-            </div>
-
-        </div>
-    )
+      <div>
+        {currentPage > 1 && (
+          <button onClick={handlePreviousPage}>Previous</button>
+        )}
+        {endIndex < products.length && (
+          <button onClick={handleNextPage}>Next</button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
