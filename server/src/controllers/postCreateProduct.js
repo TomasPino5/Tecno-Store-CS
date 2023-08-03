5becdcd4d9db80d48c3cdd6605d79f4c4a1f14e8
TomiB98 <tomasbaldi@gmail.com>
2023-08-03 11:14:14 -0300

    Ruta post pendiente de aprobar por HA

--- server/src/controllers/postCreateProduct.js
+++ server/src/controllers/postCreateProduct.js
@@ -1,16 +1,16 @@
 const Products = require('../models/product.js');
 
 const postCreateProduct = async(req, res)=>{
-    const { id, name, href, imageSrc, imageAlt, price, stock, brand, category, description } = req.body;
+    const { name, href, imageSrc, imageAlt, price, stock, brand, category, description } = req.body;
     try{
-        const dbProduct = await Products.findOne({where:{name:name}});
-        if(dbProduct){
-            res.status(200).json({message: 'Ya existe un producto con ese nombre'});
-        }
-        else{
-            const product = await Products.create({id, name, href, imageSrc, imageAlt, price, stock, brand, category, description});
+        //const dbProduct = await Products.findOne({where:{name:name}});
+        // if(dbProduct){
+        //     res.status(200).json({message: 'Ya existe un producto con ese nombre'});
+        // }
+        //else{
+            const product = await Products.create({ name, href, imageSrc, imageAlt, price, stock, brand, category, description});
             res.status(200).json(product);
-        }
+        //}
     }
     catch(error){
         res.status(500).json({message: error.message});
