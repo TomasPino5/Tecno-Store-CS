80ca816cf9343e0e7ab0b04c133fd81b43f96048
AldoTorrez <adlotorrez91@gmail.com>
2023-08-04 15:57:48 -0400

    rutal detail solucionado

--- server/src/controllers/getProductDetail.js
+++ server/src/controllers/getProductDetail.js
@@ -1,33 +1,33 @@
 const productsData = require('../data')
-//const Products = require('../models/product')
-
-// const getProductDetail = async (id) => {
-
-//     let findProduct = await Products.findOne({
-//         where: { id: id }
-//     })
-//     return findProduct
-// }
+const Products = require('../models/product')
 
 const getProductDetail = async (id) => {
 
-    let findProduct = productsData.find(product => product.id === id);
-
-    if (findProduct) {
-        return {
-            id: findProduct.id,
-            name: findProduct.name,
-            href: findProduct.href,
-            imageSrc: findProduct.imageSrc,
-            imageAlt: findProduct.imageAlt,
-            price: findProduct.price,
-            stock: findProduct.stock,
-            brand: findProduct.brand,
-            category: findProduct.category,
-            description: findProduct.description
-        };
-    }
-    return null;
+    let findProduct = await Products.findOne({
+        where: { id: id }
+    })
+    return findProduct
 }
+
+// const getProductDetail = async (id) => {
+
+//     let findProduct = productsData.find(product => product.id === id);
+
+//     if (findProduct) {
+//         return {
+//             id: findProduct.id,
+//             name: findProduct.name,
+//             href: findProduct.href,
+//             imageSrc: findProduct.imageSrc,
+//             imageAlt: findProduct.imageAlt,
+//             price: findProduct.price,
+//             stock: findProduct.stock,
+//             brand: findProduct.brand,
+//             category: findProduct.category,
+//             description: findProduct.description
+//         };
+//     }
+//     return null;
+// }
 
 module.exports = getProductDetail;
