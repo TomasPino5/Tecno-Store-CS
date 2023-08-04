e6ffb5c155525c32e832b3eab1d870ebdfd132c9
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-04 10:58:56 -0300

    css(detail: 50%)

--- client/src/components/detail/detail.jsx
+++ client/src/components/detail/detail.jsx
@@ -38,23 +38,30 @@ const Detail = () => {
               <img src={myProduct?.imageSrc} alt="img not found" />
             </div>
             <div className={style.text__container}>
-              <p>Nuevo | +1000 vendidos</p>
-              {myProduct.title ? (
-                <h3>{myProduct.title}</h3>
-              ) : (
-                <h3>{myProduct.name}</h3>
-              )}
-              <h4>Price: ${myProduct?.price}</h4>
-              <p>Ver los medios de pago</p>
-              <h4>Brand: {myProduct?.brand}</h4>
-              <h4>Category: {myProduct?.category}</h4>
-              {myProduct?.stock > 0 ? (
-                <p>Stock disponible</p>
-              ) : (
-                <p>Stock agotado</p>
-              )}
-              <p>Almacenados y enviado por nosotros</p>
-              <h4>Stock: {myProduct?.stock}</h4>
+              <p className={style.N__p}>Nuevo | +1000 vendidos</p>
+              <div className={style.N__Tc}>
+                {myProduct.title ? (
+                  <h3 className={style.N__t}>{myProduct.title}</h3>
+                ) : (
+                  <h3 className={style.N__t}>{myProduct.name}</h3>
+                )}
+              </div>
+
+              <h4 className={style.N__Pr}>Price: ${myProduct?.price}</h4>
+              <p className={style.N__p}>Ver los medios de pago</p>
+              <div className={style.N__C}>
+                <p className={style.nc}>Brand: {myProduct?.brand}</p>
+                <p className={style.nc}>Category: {myProduct?.category}</p>
+              </div>
+              <div className={style.N__St}>
+                {myProduct?.stock > 0 ? (
+                  <p>Stock disponible</p>
+                ) : (
+                  <p>Stock agotado</p>
+                )}
+                <p>Almacenados y enviado por nosotros</p>
+                <h4>Stock: {myProduct?.stock}</h4>
+              </div>
               <div className={style.color__container}>
                 <p>Color:</p>
                 <button className={style.btn1}></button>
@@ -62,8 +69,10 @@ const Detail = () => {
                 <button className={style.btn3}></button>
                 <button className={style.btn4}></button>
               </div>
-              <div>
+              <div className={style}>
                 <p>Description {myProduct?.description}</p>
+              </div>
+              <div className={style}>
                 <button>Agregar al carrito</button>
               </div>
             </div>
