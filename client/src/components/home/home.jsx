6389fcf7f0e82ce7d387e27d19c17fd90b026405
TomiB98 <tomasbaldi@gmail.com>
2023-08-04 12:50:49 -0300

    Cambios boton next y prev

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -2,6 +2,7 @@ import Cards from "../cards/cards";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts, orderByPrice, filterByBrand, filterByCategory, clearFilter } from "../../redux/actions";
+import style from './home.module.css'
 // import styles from "./home.module.css"
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
   const products = useSelector((state) => state.allProducts);
   const brands = useSelector((state) => state.brands);
   const categories = useSelector((state) => state.categories);
-    
+
   console.log(products)
 
   const dispatch = useDispatch()
@@ -36,66 +37,71 @@ const Home = () => {
 
   const recipesToDisplay = products.slice(startIndex, endIndex);
 
-    //filtros
-    const handleOrderByPrice = (event) => {
-      dispatch(orderByPrice(event.target.value));
-    };
-    
-    const handleBrandFilter = (event) => {
-      dispatch(filterByBrand(event.target.value));
-      document.getElementById("categoryFilter").value = "";
-    };
-    
-    const handleCategoryFilter = (event) => {
-      dispatch(filterByCategory(event.target.value));
-    };
+  //filtros
+  const handleOrderByPrice = (event) => {
+    dispatch(orderByPrice(event.target.value));
+  };
 
-    const handleClearFilters = () => {
-      dispatch(clearFilter());
-      // Devuelve el valor de los select al origen 
-      document.getElementById("orderByPrice").value = "";
-      document.getElementById("brandFilter").value = "";
-      document.getElementById("categoryFilter").value = "";
-    };
+  const handleBrandFilter = (event) => {
+    dispatch(filterByBrand(event.target.value));
+    document.getElementById("categoryFilter").value = "";
+  };
 
-    return (
-        <div>
-            
-            <div>
-                <select id="orderByPrice" onChange={handleOrderByPrice}>
-                    <option value="">Price</option>
-                    <option value="-+">Menor a Mayor</option>
-                    <option value="+-">Mayor a Menor</option>
-                </select>
-                <select id="brandFilter" onChange={handleBrandFilter}>
-                <option value="">Brands</option>
-                    {brands.map((brand) => (
-                        <option key={brand} value={brand}>{brand}</option>
-                    ))}
-                </select>
-                <select  id="categoryFilter" onChange={handleCategoryFilter}>
-                <option value="">Categories</option>
-                    {categories.map((category) => (
-                        <option key={category} value={category}>{category}</option>
-                    ))}
-                </select>
-                <button onClick={handleClearFilters}>Limpiar</button>
-            </div>
+  const handleCategoryFilter = (event) => {
+    dispatch(filterByCategory(event.target.value));
+  };
 
-            <div>
-                <Cards products={recipesToDisplay}/>
-            </div>
-  
+  const handleClearFilters = () => {
+    dispatch(clearFilter());
+    // Devuelve el valor de los select al origen 
+    document.getElementById("orderByPrice").value = "";
+    document.getElementById("brandFilter").value = "";
+    document.getElementById("categoryFilter").value = "";
+  };
+
+  return (
+    <div>
+
+      <div className={style.filtros}>
+        <select id="orderByPrice" onChange={handleOrderByPrice}>
+          <option value="">Price</option>
+          <option value="-+">Menor a Mayor</option>
+          <option value="+-">Mayor a Menor</option>
+        </select>
+        <select id="brandFilter" onChange={handleBrandFilter}>
+          <option value="">Brands</option>
+          {brands.map((brand) => (
+            <option key={brand} value={brand}>{brand}</option>
+          ))}
+        </select>
+        <select id="categoryFilter" onChange={handleCategoryFilter}>
+          <option value="">Categories</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>{category}</option>
+          ))}
+        </select>
+        <button onClick={handleClearFilters}>Limpiar</button>
+      </div>
+
       <div>
-        {currentPage > 1 && (
-          <button onClick={handlePreviousPage}>Previous</button>
+        <Cards products={recipesToDisplay} />
+      </div>
+
+      <div>
+
+      <button disabled={currentPage === 1} className={style.btnPrevious} onClick={handlePreviousPage}>Prev</button>
+      <button disabled={endIndex > products.length} className={style.btnNext} onClick={handleNextPage}>Next</button>
+        {/* {currentPage > 1 && (
+          <button className={style.btnPrevious} onClick={handlePreviousPage}>Prev</button>
         )}
         {endIndex < products.length && (
-          <button onClick={handleNextPage}>Next</button>
-        )}
+          <button className={style.btnNext} onClick={handleNextPage}>Next</button>
+        )} */}
+
       </div>
     </div>
   );
 };
-
+//disabled={currentPage > 1}
+//disabled={endIndex < products.length}
 export default Home;
