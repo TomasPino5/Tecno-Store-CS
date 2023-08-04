bc284894f532f7f84d62a428ab6f7ebc47211a4d
MarianoMenseguez <marianomenseguez@hotmail.com>
2023-08-04 15:06:59 -0300

    Form creado y conectado con la ruta postProduct

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -25,12 +25,12 @@ const Form = () => {
   const [priceError, setPriceError] = useState("");
   const [stockError, setStockError] = useState("");
   const [brandError, setBrandError] = useState("");
-  const [imageSrc, setImageSrcError] = useState("")
+  const [imageSrc, setImageSrcError] = useState("");
   const [categoryError, setCategoryError] = useState("");
   const [descriptionError, setDescriptionError] = useState("");
-  const [imageAltError, setImageAltError] = useState("")
-  const [hrefError, setHrefError] = useState("")
-  const [minError, setMinError] = useState("")
+  const [imageAltError, setImageAltError] = useState("");
+  const [hrefError, setHrefError] = useState("");
+  const [minError, setMinError] = useState("");
 
   const validateName = (name) => {
     setNameError(name.trim() === "" ? "¡Se requiere el nombre!" : "");
@@ -56,34 +56,53 @@ const Form = () => {
     setBrandError(brand.trim() === "" ? "¡Se requiere la marca!" : "");
   };
 
-  const validateImg = (imageSrc) => {
+  const validateImgSrc = (imageSrc) => {
     setImageSrcError(
-      /^.*\.(jpg|jpeg|png|gif)$/.test(imageSrc) ?
-        "" : "¡Por favor ingresa una img válida!"
-    )
-  }
+      /(http(s?):)([/|.|\w|\s|-])*.(?:jpg|gif|png)/.test(imageSrc)
+        ? ""
+        : "¡Por favor ingresa una img válida!"
+    );
+  };
 
   const validateCategory = (category) => {
-    setCategoryError(category.trim() === "" ? "¡Se requiere la categoría!" : "");
+    setCategoryError(
+      category.trim() === "" ? "¡Se requiere la categoría!" : ""
+    );
   };
 
   const validateDescription = (description) => {
-    setDescriptionError(description.trim() === "" ? "¡Se requiere la descripción!" : "");
+    setDescriptionError(
+      description.trim() === "" ? "¡Se requiere la descripción!" : ""
+    );
+  };
+  // aca agrego las 3 faltantes
+  const validateImgAlt = (imageAlt) => {
+    setImageAltError(
+      imageAlt.trim() === "" ? "¡Se requiere el Alt de la imagen!" : ""
+    );
+  };
+
+  const validateHref = (href) => {
+    setHrefError(href.trim() === "" ? "¡Se requiere href!" : "");
+  };
+  const validateMin = (min) => {
+    setMinError(min.trim() === "" ? "¡Se requiere el min!" : "");
   };
 
   const changeHandler = (event) => {
     const property = event.target.name;
     const value = event.target.value;
-    
-    setForm({  
+
+    setForm({
       ...form,
       [property]: value,
     });
-    
+
     switch (property) {
       case "name":
         validateName(value);
         break;
+
       case "price":
         validatePrice(value);
         break;
@@ -100,7 +119,16 @@ const Form = () => {
         validateDescription(value);
         break;
       case "imageSrc":
-        validateImg(value)
+        validateImgSrc(value);
+        break;
+      case "min":
+        validateMin(value);
+        break;
+      case "imageAlt":
+        validateImgAlt(value);
+        break;
+      case "href":
+        validateHref(value);
         break;
       default:
         break;
@@ -130,17 +158,17 @@ const Form = () => {
     <div>
       <div className={style.buttonReturn}>
         <Link to="/">
-          <button className={style.button}>Volver</button>
+          <button className={style.button}>Return</button>
         </Link>
       </div>
       <div className={style.card}>
         <span className={style.card__title} id="title">
-          ¡Crea tu propio producto!
+          Create your own product
         </span>
 
         <form onSubmit={(e) => submitHandler(e)} className={style.Formulario}>
           <div className={style.card__form}>
-            <label className={style.label__form}>Nombre del Producto: </label>
+            <label className={style.label__form}>Name of product: </label>
             <input
               type="text"
               value={form.name}
@@ -148,10 +176,12 @@ const Form = () => {
               name="name"
               placeholder="Escribe el nombre del producto..."
             />
-            {nameError && <strong className={style.card__content}>{nameError}</strong>}
+            {nameError && (
+              <strong className={style.card__content}>{nameError}</strong>
+            )}
           </div>
           <div className={style.card__form}>
-            <label className={style.label__form}>Imagen del Producto: </label>
+            <label className={style.label__form}>Image of product: </label>
             <input
               type="text"
               value={form.imageSrc}
@@ -159,10 +189,12 @@ const Form = () => {
               name="imageSrc"
               placeholder="Ingresa la URL de la imagen..."
             />
-            {imageSrc && <strong className={style.card__content}>{imageSrc}</strong>}
+            {imageSrc && (
+              <strong className={style.card__content}>{imageSrc}</strong>
+            )}
           </div>
           <div className={style.card__form}>
-            <label className={style.label__form}>Precio: </label>
+            <label className={style.label__form}>Price: </label>
             <input
               type="text"
               value={form.price}
@@ -170,7 +202,9 @@ const Form = () => {
               name="price"
               placeholder="Escribe el precio del producto..."
             />
-            {priceError && <strong className={style.card__content}>{priceError}</strong>}
+            {priceError && (
+              <strong className={style.card__content}>{priceError}</strong>
+            )}
           </div>
           <div className={style.card__form}>
             <label className={style.label__form}>Stock: </label>
@@ -181,10 +215,12 @@ const Form = () => {
               name="stock"
               placeholder="Escribe la cantidad de stock..."
             />
-            {stockError && <strong className={style.card__content}>{stockError}</strong>}
+            {stockError && (
+              <strong className={style.card__content}>{stockError}</strong>
+            )}
           </div>
           <div className={style.card__form}>
-            <label className={style.label__form}>Marca: </label>
+            <label className={style.label__form}>Brand: </label>
             <input
               type="text"
               value={form.brand}
@@ -192,10 +228,12 @@ const Form = () => {
               name="brand"
               placeholder="Escribe la marca del producto..."
             />
-            {brandError && <strong className={style.card__content}>{brandError}</strong>}
+            {brandError && (
+              <strong className={style.card__content}>{brandError}</strong>
+            )}
           </div>
           <div className={style.card__form}>
-            <label className={style.label__form}>Categoría: </label>
+            <label className={style.label__form}>Category: </label>
             <input
               type="text"
               value={form.category}
@@ -203,10 +241,12 @@ const Form = () => {
               name="category"
               placeholder="Escribe la categoría del producto..."
             />
-            {categoryError && <strong className={style.card__content}>{categoryError}</strong>}
+            {categoryError && (
+              <strong className={style.card__content}>{categoryError}</strong>
+            )}
           </div>
           <div className={style.card__form}>
-            <label className={style.label__form}>Descripción: </label>
+            <label className={style.label__form}>Description: </label>
             <input
               type="text"
               value={form.description}
@@ -214,8 +254,59 @@ const Form = () => {
               name="description"
               placeholder="Escribe la descripción del producto..."
             />
-            {descriptionError && <strong className={style.card__content}>{descriptionError}</strong>}
+            {descriptionError && (
+              <strong className={style.card__content}>
+                {descriptionError}
+              </strong>
+            )}
           </div>
+
+          {/* ACA VAN LAS 3 PROPIEDADES FALTANTES  */}
+
+          {/* HREF */}
+          <div className={style.card__form}>
+            <label className={style.label__form}>Href: </label>
+            <input
+              type="text"
+              value={form.href}
+              onChange={(e) => changeHandler(e)}
+              name="href"
+              placeholder="Escribe el href del producto..."
+            />
+            {hrefError && (
+              <strong className={style.card__content}>{hrefError}</strong>
+            )}
+          </div>
+
+          {/* imageAlt */}
+          <div className={style.card__form}>
+            <label className={style.label__form}>ImageAlt: </label>
+            <input
+              type="text"
+              value={form.imageAlt}
+              onChange={(e) => changeHandler(e)}
+              name="imageAlt"
+              placeholder="Escribe el imageAlt del producto..."
+            />
+            {imageAltError && (
+              <strong className={style.card__content}>{imageAltError}</strong>
+            )}
+          </div>
+          {/* min */}
+          <div className={style.card__form}>
+            <label className={style.label__form}>Min: </label>
+            <input
+              type="text"
+              value={form.minError}
+              onChange={(e) => changeHandler(e)}
+              name="min"
+              placeholder="Escribe el Min del producto..."
+            />
+            {minError && (
+              <strong className={style.card__content}>{minError}</strong>
+            )}
+          </div>
+
           <div calssName={style.btn}>
             <button
               disabled={
@@ -223,12 +314,15 @@ const Form = () => {
                 priceError ||
                 categoryError ||
                 brandError ||
-                descriptionError
+                descriptionError ||
+                imageAltError ||
+                hrefError ||
+                minError
               }
               type="submit"
               className={style.btn}
             >
-              Crear Producto
+              Create product
             </button>
           </div>
         </form>
