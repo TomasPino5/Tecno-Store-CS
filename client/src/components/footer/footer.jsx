71573a8cde7edc85c6720b96fece6ed34c44f1e4
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-07 11:11:05 -0300

    css

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -6,22 +6,41 @@ const Footer = () => {
         <div className={style.colum1}>
           <p className={style.colum1_p}>Mas informacion sobre Tecno Store SC</p>
           <span className={style.colum1_spn}>
-          ¡Descubre la tecnología en su máxima expresión en Tecno Store C! Encuentra los últimos smartphones, auriculares de alta calidad y accesorios esenciales, todo en un solo lugar. Tu destino definitivo para mantenerte conectado y disfrutar al máximo de tus dispositivos. ¡Bienvenido a la revolución tecnológica!
+            ¡Descubre la tecnología en su máxima expresión en Tecno Store CS!
+            Encuentra los últimos smartphones, auriculares de alta calidad y
+            accesorios esenciales, todo en un solo lugar. Tu destino definitivo
+            para mantenerte conectado y disfrutar al máximo de tus dispositivos.
+            ¡Bienvenido a la revolución tecnológica!
           </span>
         </div>
         <div className={style.column3}>
           <p className={style.colum1_p}>Informacion de contactos</p>
           <div className={style.row2}>
             <img src="https://i.ibb.co/WvGMRvx/ubicacion-logo.png" alt="" />
-            <p>Rosario-Santa Fe-Argentina</p>
+            <a
+              className={style.link}
+              href="https://www.google.com/maps?output=search&q=rosario+santa+fe&source=lnms&entry=mc&sa=X&ved=2ahUKEwiGpfv9tMqAAxUOuZUCHa2WAlIQ0pQJegQIDRAB"
+            >
+              Rosario-Santa Fe-Argentina
+            </a>
           </div>
           <div className={style.row2}>
             <img src="https://i.ibb.co/4VJwHrg/whatsapp-logo.png" alt="" />
-            <p>+54 341 658-6272</p>
+            <a
+              className={style.link}
+              href="https://wa.me/message/KEAE2GLG7GLQD1 "
+            >
+              +54 341 658-6272
+            </a>
           </div>
           <div className={style.row2}>
             <img src="https://i.ibb.co/nCbxwm9/gmail-icono.png" alt="" />
-            <p>tecnostorecs55@gmail.com</p>
+            <a
+              className={style.link}
+              href="https://mail.google.com/mail/u/1/#inbox"
+            >
+              tecnostorecs55@gmail.com
+            </a>
           </div>
         </div>
         <div className={style.column2}>
@@ -45,11 +64,13 @@ const Footer = () => {
             </div>
             <div className={style.row}>
               <img src="https://i.ibb.co/ynz3ySx/ico-github.png" alt="" />
-              <a href="https://github.com/AldoTorrez">Aldo-Torrez</a>
+              <a href="https://github.com/AldoTorrez">Aldo-Torres</a>
             </div>
             <div className={style.row}>
               <img src="https://i.ibb.co/ynz3ySx/ico-github.png" alt="" />
-              <a href="https://github.com/MarianoMenseguez">Mariano-Mesenguez</a>
+              <a href="https://github.com/MarianoMenseguez">
+                Mariano-Mesenguez
+              </a>
             </div>
             <div className={style.row}>
               <img src="https://i.ibb.co/ynz3ySx/ico-github.png" alt="" />
