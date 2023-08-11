6c91857b1d6f0e5b800fd2ac182c0985b8a0ce5e
TomasPino5 <tomaspino.velez@gmail.com>
2023-08-11 19:33:40 -0300

    index

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-// const server = require('./src/app');
-// const sequelize = require('./src/db');
-// require('./src/models/product');
-// require('./src/models/users');
-
-// sequelize.sync({ force: false }).then(() => { //true
-//   server.listen(3001, () => {
-//     console.log('%s listening at 3001')
-//   });
-// });
-
+const server = require('./src/app');
+const sequelize = require('./src/db');
+require('./src/models/product');
+require('./src/models/users');
 require('dotenv').config();
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => { //true
   server.listen(port, () => {
-  console.log(`Server raised in port ${port}`);
+    console.log('%s listening at 3001')
   });
 });
+
+// require('dotenv').config();
+// const server = require('./src/app.js');
+// const port = process.env.PORT || 3001;
+// const sequelize = require('./src/db');
+
+// sequelize.sync({ force: true }).then(() => {
+//   server.listen(port, () => {
+//   console.log(`Server raised in port ${port}`);
+//   });
+// });
