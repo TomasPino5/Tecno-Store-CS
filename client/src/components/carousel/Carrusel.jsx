1b878f37d5cdc311e79bda92c66dc9f3cd7403ac
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-11 13:02:01 -0300

    estilos de la pagina

--- client/src/components/carousel/Carrusel.jsx
+++ client/src/components/carousel/Carrusel.jsx
@@ -26,128 +26,131 @@ const Carrusel = () => {
 
   return (
     <div className="div-construction pt-0 flex flex-col justify-start items-center">
-      <div className={style.marg}>
-        <Hero />
-      </div>
-      <div className={style.img_carrucel_center}>
-        <div className={style.img_carrucel}>
-          <ul>
-            <li>
-              <img src={img1} alt="" />
-            </li>
+      <div className={style.over}>
+        <div className={style.marg}>
+          <Hero />
+        </div>
+        <div className={style.img_carrucel_center}>
+          <div className={style.img_carrucel}>
+            <ul>
+              <li>
+                <img src={img1} alt="" />
+              </li>
 
-            <li>
-              <img src={img3} alt="" />
-            </li>
-            <li>
-              <img src={img4} alt="" />
-            </li>
-            <li>
-              <img src={img5} alt="" />
-            </li>
-            <li>
-              <img src={img6} alt="" />
-            </li>
-          </ul>
+              <li>
+                <img src={img3} alt="" />
+              </li>
+              <li>
+                <img src={img4} alt="" />
+              </li>
+              <li>
+                <img src={img5} alt="" />
+              </li>
+              <li>
+                <img src={img6} alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={style.button_filter}>
+          <Link to={{ pathname: "/products" }}>
+            <button>
+              <img src="https://i.ibb.co/Z89607V/logo-motorola.jpg" alt="" />
+            </button>
+          </Link>
+
+          <Link to={{ pathname: "/products" }}>
+            <button>
+              <img src="https://i.ibb.co/bgptHWh/logo-samsung.jpg" alt="" />
+            </button>
+          </Link>
+
+          <Link to={{ pathname: "/products" }}>
+            <button>
+              <img src="https://i.ibb.co/FVv7FRZ/logo-iphone.jpg" alt="" />
+            </button>
+          </Link>
+
+          <Link to={{ pathname: "/products" }}>
+            <button>
+              <img src="https://i.ibb.co/KGJtsS0/logo-xiaomi.jpg" alt="" />
+            </button>
+          </Link>
+        </div>
+        <div>
+          <Link
+            style={{ textDecoration: "none", color: "blue" }}
+            to={{ pathname: "/products" }}
+            className={style.linkWrapper}
+          >
+            <p className={style.pp}>Mira todos nuestros productos</p>
+          </Link>
+        </div>
+        <div className={style.aboutSection}>
+          <p className={style.aboutText}>
+            Somos una empresa comprometida con la calidad y excelencia en la
+            industria de la tecnología. Ofrecemos una amplia gama de productos
+            de alta calidad que satisfacen las necesidades de nuestros clientes.
+          </p>
+        </div>
+        <div className={style.aboutSectionDer}>
+          <p className={style.aboutText}>
+            Además, contamos con un equipo de profesionales capacitados que se
+            dedican a brindar un servicio excepcional en la reparación y
+            mantenimiento de dispositivos móviles.
+          </p>
+        </div>
+        <div className={style.aboutSection}>
+          <p className={style.aboutText}>
+            En Tecno Store CS, nos esforzamos por ser líderes en la industria y
+            proporcionar soluciones confiables y eficientes para todos nuestros
+            clientes.
+          </p>
+        </div>
+        <div className={style.slider__position}>
+          <Slider className="slider" {...settings}>
+            <Link to="/products">
+              <div className="img2">
+                <img
+                  className="img"
+                  src="https://i.ibb.co/Z89607V/logo-motorola.jpg"
+                  alt="logo motorola "
+                />
+              </div>
+            </Link>
+
+            <Link to="/products">
+              <div className="img2">
+                <img
+                  className="img"
+                  src="https://i.ibb.co/bgptHWh/logo-samsung.jpg"
+                  alt="logo samsung"
+                />
+              </div>
+            </Link>
+
+            <Link to="/products">
+              <div className="img2">
+                <img
+                  className="img"
+                  src="https://i.ibb.co/FVv7FRZ/logo-iphone.jpg"
+                  alt="logo apple"
+                />
+              </div>
+            </Link>
+
+            <Link to="/products">
+              <div className="img2">
+                <img
+                  className="img"
+                  src="https://i.ibb.co/KGJtsS0/logo-xiaomi.jpg"
+                  alt="logo xiaomi"
+                />
+              </div>
+            </Link>
+          </Slider>
         </div>
       </div>
-      <div className={style.button_filter}>
-        <Link to={{ pathname: "/products" }}>
-          <button>
-            <img src="https://i.ibb.co/Z89607V/logo-motorola.jpg" alt="" />
-          </button>
-        </Link>
-
-        <Link to={{ pathname: "/products" }}>
-          <button>
-            <img src="https://i.ibb.co/bgptHWh/logo-samsung.jpg" alt="" />
-          </button>
-        </Link>
-
-        <Link to={{ pathname: "/products" }}>
-          <button>
-            <img src="https://i.ibb.co/FVv7FRZ/logo-iphone.jpg" alt="" />
-          </button>
-        </Link>
-
-        <Link to={{ pathname: "/products" }}>
-          <button>
-            <img src="https://i.ibb.co/KGJtsS0/logo-xiaomi.jpg" alt="" />
-          </button>
-        </Link>
-      </div>
-      <div>
-        <Link
-          style={{ textDecoration: "none", color: "blue" }}
-          to={{ pathname: "/products" }}
-          className={style.linkWrapper}
-        >
-          <p className={style.pp}>Mira todos nuestros productos</p>
-        </Link>
-      </div>
-      <div className={style.aboutSection}>
-        <p className={style.aboutText}>
-          Somos una empresa comprometida con la calidad y excelencia en la
-          industria de la tecnología. Ofrecemos una amplia gama de productos de
-          alta calidad que satisfacen las necesidades de nuestros clientes.
-        </p>
-      </div>
-      <div className={style.aboutSectionDer}>
-        <p className={style.aboutText}>
-          Además, contamos con un equipo de profesionales capacitados que se
-          dedican a brindar un servicio excepcional en la reparación y
-          mantenimiento de dispositivos móviles.
-        </p>
-      </div>
-      <div className={style.aboutSection}>
-        <p className={style.aboutText}>
-          En Tecno Store CS, nos esforzamos por ser líderes en la industria y
-          proporcionar soluciones confiables y eficientes para todos nuestros
-          clientes.
-        </p>
-      </div>
-
-      <Slider className="slider" {...settings}>
-        <Link to="/products">
-          <div className="img2">
-            <img
-              className="img"
-              src="https://i.ibb.co/Z89607V/logo-motorola.jpg"
-              alt="logo motorola "
-            />
-          </div>
-        </Link>
-
-        <Link to="/products">
-          <div className="img2">
-            <img
-              className="img"
-              src="https://i.ibb.co/bgptHWh/logo-samsung.jpg"
-              alt="logo samsung"
-            />
-          </div>
-        </Link>
-
-        <Link to="/products">
-          <div className="img2">
-            <img
-              className="img"
-              src="https://i.ibb.co/FVv7FRZ/logo-iphone.jpg"
-              alt="logo apple"
-            />
-          </div>
-        </Link>
-
-        <Link to="/products">
-          <div className="img2">
-            <img
-              className="img"
-              src="https://i.ibb.co/KGJtsS0/logo-xiaomi.jpg"
-              alt="logo xiaomi"
-            />
-          </div>
-        </Link>
-      </Slider>
     </div>
   );
 };
