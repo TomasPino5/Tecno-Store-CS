b3f291699cd181901da2744522d6c28c93455b75
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-15 15:32:22 -0300

    card DarkMode

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import style from "./card.module.css";
+import { useSelector } from "react-redux";
 // import { useDispatch } from "react-redux";
 // import { deleteProductsById } from "../../redux/actions";
 
@@ -23,22 +24,30 @@ export default function Card({
   const formatPriceWithDots = (price) => {
     return price.toLocaleString();
   };
+  const darkMode = useSelector((state) => state.darkMode); // Agrega esta línea
+  // className={darkMode ? style.darkMode : style.lightMode}
 
   return (
-    <div className={style.card}>
+    <div className={darkMode ? style.carddarkMode : style.card}>
       <NavLink
         to={`/product/${id}`}
         style={{ textDecoration: "none", color: "inherit" }}
       >
-        <div className={style.img_container}>
+        <div
+          className={
+            darkMode ? style.img_containerdarkMode : style.img_container
+          }
+        >
           <img className={style.card__img} src={imageSrc} alt={imageAlt} />
         </div>
         <div className={style.container_name}>
-          <p className={style.ht}>{name}</p>
+          <p className={darkMode ? style.htdarkMode : style.ht}>{name}</p>
         </div>
         <div className={style.container_data}>
           {/* <span className={style.price}>${price}</span> */}
-          <span className={style.price}>${formatPriceWithDots(price)}</span>
+          <span className={darkMode ? style.pricedarkMode : style.price}>
+            ${formatPriceWithDots(price)}
+          </span>
           <p className={style.texts}>{brand}</p>
           <p className={style.texts}>{category}</p>
         </div>
