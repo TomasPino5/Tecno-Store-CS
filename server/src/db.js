fc48843cae52d4f4cfee8eb9b824c962031c789b
TomasPino5 <tomaspino.velez@gmail.com>
2023-08-16 17:37:16 -0300

    deploy1

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_DEPLOY,
 } = process.env;
 
-const sequelize = new Sequelize('Store', DB_USER, DB_PASSWORD, {
+// const sequelize = new Sequelize('store', DB_USER, DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false
+// });
+
+const sequelize = new Sequelize(DB_DEPLOY, {
   host: 'localhost',
   dialect: 'postgres',
   logging: false
