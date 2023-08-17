7df9a10e362ed46b34244e599aae75c310837cd2
TomasPino5 <tomaspino.velez@gmail.com>
2023-08-17 11:39:19 -0300

    arreglado lo de railway

--- server/index.js
+++ server/index.js
@@ -2,22 +2,20 @@ const server = require('./src/app');
 const sequelize = require('./src/db');
 require('./src/models/product');
 require('./src/models/users');
-require('dotenv').config();
-const port = process.env.PORT || 3001;
+
+//railway
+// require('dotenv').config();
+// const port = process.env.PORT || 3001;
 
 sequelize.sync({ force: false }).then(() => { //true
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`)
+  server.listen(3001, () => {
+    console.log('%s listening at 3001')
   });
 });
 
-// require('dotenv').config();
-// const server = require('./src/app.js');
-// const port = process.env.PORT || 3001;
-// const sequelize = require('./src/db');
-
-// sequelize.sync({ force: true }).then(() => {
+//railway
+// sequelize.sync({ force: false }).then(() => { //true
 //   server.listen(port, () => {
-//   console.log(`Server raised in port ${port}`);
+//     console.log(`%s listening at ${port}`)
 //   });
 // });
