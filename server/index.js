8c74e6914d789fe1edad2d04b335ab47621c2e4b
Tomas Pino <tomaspino48@gmail.com>
2023-08-18 12:05:18 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,18 +4,19 @@ require('./src/models/product');
 require('./src/models/users');
 
 //railway
-// require('dotenv').config();
-// const port = process.env.PORT || 3001;
+require('dotenv').config();
+const port = process.env.PORT || 3001;
 
-sequelize.sync({ force: false }).then(() => { //true
-  server.listen(3001, () => {
-    console.log('%s listening at 3001')
-  });
-});
+//localhost
+// sequelize.sync({ force: false }).then(() => { //true
+// server.listen(3001, () => {
+// console.log('%s listening at 3001')
+// });
+// });
 
 //railway
-// sequelize.sync({ force: false }).then(() => { //true
-//   server.listen(port, () => {
-//     console.log(`%s listening at ${port}`)
-//   });
-// });
+sequelize.sync({ force: false }).then(() => { //true
+   server.listen(port, () => {
+     console.log(`%s listening at ${port}`)
+   });
+ });
