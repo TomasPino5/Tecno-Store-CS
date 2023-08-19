a62b29813b5800fe8f3ed4fea3756118402edc71
SebastianBeltzer <124115763+SebastianBeltzer@users.noreply.github.com>
2023-08-19 18:56:06 -0300

    serchbar Admin, dark Admin, cart Card

--- client/src/components/listCompras/ListCompras.jsx
+++ client/src/components/listCompras/ListCompras.jsx
@@ -1,32 +1,31 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCompras } from "../../redux/actions";
-import listCompras from './listCompras.css';
+import listCompras from "./listCompras.css";
 
-const ListCompras = ()=>{
+const ListCompras = () => {
+  const dispatch = useDispatch();
+  const compras = useSelector((state) => state.getCompras);
+  const darkMode = useSelector((state) => state.darkMode); // Agrega esta línea
+  // className={darkMode ? style.darkmode : style.lightMode}
 
-    const dispatch = useDispatch();
-    const compras = useSelector((state)=> state.getCompras);
-
-    useEffect(()=>{
-        dispatch(getCompras());
-    },[])
-    return(
-        <div className="container-compras">
-            {
-                compras.map((prop,key)=>{
-                    return(
-                        <div key={key} className="compras">
-                            <p>{prop.user.email}</p>
-                            <p>{prop.user.direction}</p>
-                            <p>{prop.user.telefone}</p>
-                            <p>{prop.products[0].name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    dispatch(getCompras());
+  }, []);
+  return (
+    <div className="container-compras">
+      {compras.map((prop, key) => {
+        return (
+          <div key={key} className={darkMode ? "comprasdark" : "compras"}>
+            <p>{prop.user.email}</p>
+            <p>{prop.user.direction}</p>
+            <p>{prop.user.telefone}</p>
+            <p>{prop.products[0].name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default ListCompras;
--- client/src/components/listusers/listusers.jsx
+++ client/src/components/listusers/listusers.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllUsers, userActive, userAdmin } from "../../redux/actions";
-import './listUsers.css';
-import swal from 'sweetalert';
-
+import "./listUsers.css";
+import swal from "sweetalert";
 
 const Listusers = () => {
   const dispatch = useDispatch();
   const allusers = useSelector((state) => state.getallusers);
   allusers.sort((a, b) => a.id - b.id);
+  const darkMode = useSelector((state) => state.darkMode); // Agrega esta línea
+  // className={darkMode ? style.darkmode : style.lightMode}
 
   useEffect(() => {
     dispatch(getAllUsers());
@@ -41,28 +42,40 @@ const Listusers = () => {
     });
   };
 
-  return(
+  return (
     <div className="container-users">
-      {
-        allusers.map((user, key)=>{
-          return(
-            <div className="users" key={key}>
-              <p>{user.name}</p>
-              <p>{user.email}</p>
-              <p>{user.isActive === true? 'true':'false'}</p>
-              <p>{user.admin === true? 'true':'false'}</p>
-              <p>
-              <button className="modify" onClick={()=>{handleModidyUser(user.id)}}>{user.isActive === true? 'Desactivar':'Activar'}</button>
-              </p>
-              <p>
-              <button className="modify" onClick={()=>{handleUserAdmin(user.id)}}>admin</button>
-              </p>
-            </div>
-          )
-        })
-      }
+      {allusers.map((user, key) => {
+        return (
+          <div className={darkMode ? "usersdark" : "users"} key={key}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.isActive === true ? "true" : "false"}</p>
+            <p>{user.admin === true ? "true" : "false"}</p>
+            <p>
+              <button
+                className="modify"
+                onClick={() => {
+                  handleModidyUser(user.id);
+                }}
+              >
+                {user.isActive === true ? "Desactivar" : "Activar"}
+              </button>
+            </p>
+            <p>
+              <button
+                className="modify"
+                onClick={() => {
+                  handleUserAdmin(user.id);
+                }}
+              >
+                admin
+              </button>
+            </p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default Listusers;
