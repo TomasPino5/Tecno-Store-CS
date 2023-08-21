95ee4b66fdc076bc87ab72b7c53c29273727e7fe
TomiB98 <tomasbaldi@gmail.com>
2023-08-21 10:19:53 -0300

    modificacion pago comprar directo

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -199,7 +199,7 @@ const reducer = (state = initialState, action) => {
 
       if (productInCart) {
         // Verifica si la cantidad en el carrito + 1 no excede el stock máximo
-        if (productInCart.quantity + 1 <= productInCart.stock) {
+        if (productInCart.quantity <= productInCart.stock) { // productInCart.quantity + 1
           const updatedItems = state.items.map((item) =>
             item.id === id ? { ...item, quantity: item.quantity + 1 } : item
           );
